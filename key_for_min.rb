ddc24d9d19d5b9ab834a922fe3309c7c7c7a66da
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 12:26:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  ref_value = 100000 #name_hash.values[0]
+  ref_value = name_hash.values[0]
   temp = {}
   name_hash.collect { |key, value| 
     if value < ref_value
