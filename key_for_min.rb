4508abfb58947884beb95f55cc23c036952565d8
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 12:28:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,15 @@ def key_for_min_value(name_hash)
   ref_value = 1000000 #name_hash.values[0]
   temp = {}
   if block_given?
-  name_hash.collect { |key, value| 
-    if value < ref_value
-      temp[key] = value
-      ref_value = value
-    end
-  }
-  temp.to_a.last[0]
-else
-  
+   name_hash.collect { |key, value| 
+      if value < ref_value
+        temp[key] = value
+        ref_value = value
+      end
+      }
+    temp.to_a.last[0]
+  else
+    nil
+  end
 end
 
